4e58a2077e6bdad343f6285f90733658de5c0647
Ignacio Holguín <ignhol211@alu.centroafuera.es>
2021-10-21 12:46:30 +0200

    EjercicioHilos2 sin terminar

---
+++ src/EjercicioHilos2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class EjercicioHilos2 {
+
+    static ArrayList<Hilo2> listaDeHilos = new ArrayList();
+
+    public static void main(String [] args){
+
+        for (int i=0;i<100;i++){
+            Hilo2 h = new Hilo2 (i);
+            h.start();
+        }
+
+        System.out.println(escribirFinalistas());
+        System.out.println(escribirSubCampeones());
+
+    }
+
+    public static String escribirFinalistas(){
+        String result="Hilos campeones\n";
+        return result;
+    }
+
+    public static String escribirSubCampeones(){
+        String result="Hilos subcampeones\n";
+        return result;
+    }
+}
---
+++ src/Hilo2.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Hilo2 extends Thread {
+
+    public int num;
+    public int vecesQueDuerme=0;
+
+    public Hilo2(int n){
+        this.num=n;
+    }
+
+    @Override
+    public void run(){
+        Random r = new Random();
+        try {
+            Thread.sleep(1000);
+            vecesQueDuerme++;
+            byte aleatorio=(byte)r.nextInt(2);
+            while(aleatorio!=0){
+                Thread.sleep(aleatorio);
+                vecesQueDuerme++;
+                aleatorio=(byte)r.nextInt(2);}
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Soy el hilo "+num+" y he dormido "+vecesQueDuerme+" veces");
+    }
+}
