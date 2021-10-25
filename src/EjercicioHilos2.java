183ef78d3eca9c0a2abaff3e9a35eb0f759952db
ignhol211 <90613502+ignhol211@users.noreply.github.com>
2021-10-25 23:56:07 +0200

    Terminado

--- src/EjercicioHilos2.java
+++ src/EjercicioHilos2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class EjercicioHilos2 {
 
-    static ArrayList<Hilo2> listaDeHilos = new ArrayList();
+    static ArrayList<Hilo2> listaDeHilos = new ArrayList <>();
 
     public static void main(String[] args) {
 
