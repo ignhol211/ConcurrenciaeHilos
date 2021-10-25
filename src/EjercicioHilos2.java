e9eed2a95a8ae7b2e064c82333a97c715ae7b822
ignhol211 <90613502+ignhol211@users.noreply.github.com>
2021-10-25 23:52:39 +0200

    Terminado

--- src/EjercicioHilos2.java
+++ src/EjercicioHilos2.java
@@ -9,22 +9,61 @@ public class EjercicioHilos2 {
         for (int i = 0; i < 100; i++) {
             Hilo2 h = new Hilo2(i);
             h.start();
+            listaDeHilos.add(h);
         }
 
-        //System.out.println(escribirFinalistas());
-        //System.out.println(escribirSubCampeones());
+        System.out.println(escribirFinalistas());
+        System.out.println(escribirSubCampeones());
 
     }
 
     public static String escribirFinalistas() {
-
-        String result = "Hilos campeones\n";
-
-        return result;
+        return imprimir("Hilos finalistas:\n",buscarMasEjecutado());
     }
 
     public static String escribirSubCampeones() {
-        String result = "Hilos subcampeones\n";
+        int masEjecutado=buscarMasEjecutado();
+        int segundoMasEjectuado=0;
+
+        byte cont=0;
+        boolean encontrado=false;
+        do{
+            cont++;
+            for (Hilo2 hilo: listaDeHilos) {
+                if(hilo.vecesQueDuerme==masEjecutado-cont) {
+                    segundoMasEjectuado = hilo.vecesQueDuerme;
+                    encontrado=true;
+                }
+            }
+        }while(!encontrado);
+
+        return imprimir("Hilos subcampeones:\n",segundoMasEjectuado);
+    }
+
+    public static int buscarMasEjecutado(){
+        int masEjecutado=0;
+
+        for (Hilo2 hilo: listaDeHilos) {
+            try {
+                hilo.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(hilo.vecesQueDuerme>=masEjecutado)
+                masEjecutado = hilo.vecesQueDuerme;
+        }
+
+        return masEjecutado;
+    }
+
+    public static String imprimir(String cabecera,int limiteImpresion){
+        String result=cabecera;
+
+        for (Hilo2 hilo: listaDeHilos) {
+            if(hilo.vecesQueDuerme==limiteImpresion)
+                result=result.concat("El hilo "+hilo.num+" se ha ejecutado "+hilo.vecesQueDuerme+" veces\n");
+        }
+
         return result;
     }
 }
